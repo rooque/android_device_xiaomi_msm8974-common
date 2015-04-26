b32051bb84c695f46f46be0cc9fbf07e32b7bd9c
rooque <victor.rooque@gmail.com>
2015-04-26 02:28:29 -0300

    Trying to Fix BT

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,10 +1,6 @@
 /*
- *
- *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution, Apache license notifications and license are retained
- *  for attribution purposes only.
- *
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,14 +18,15 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QCOM_SW TRUE
+#define BTM_DEF_LOCAL_NAME   "Xiaomi Mi4"
 
-#define BTM_DEF_LOCAL_NAME   "Xiaomi MI4LTE"
+#define BLUETOOTH_QTI_SW TRUE
 // Enables Interleave scan
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-#define MAX_L2CAP_CHANNELS    14
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS    16
+// Skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
 #endif
+
