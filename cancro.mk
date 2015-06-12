7ba112f3cf608de28de9363eec4a2ed64217e8aa
rooque <victor.rooque@gmail.com>
2015-06-12 13:16:54 -0300

    TEST: Disable some sensor for now

--- cancro.mk
+++ cancro.mk
@@ -252,23 +252,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.pcm.enable=false
 
 #Enable more sensor
+#    ro.qualcomm.sensors.qmd=true \
+#    ro.qualcomm.sensors.smd=true \
+#    ro.qualcomm.sensors.cmc=true \
+#    ro.qualcomm.sensors.vmd=true \
+#    ro.qualcomm.sensors.gtap=true \
+#    ro.qualcomm.sensors.pedometer=true \
+#    ro.qualcomm.sensors.pam=true \
+#    ro.qualcomm.sensors.scrn_ortn=true \
+#    ro.qualcomm.sensors.georv=true \
+#    ro.qualcomm.sensors.game_rv=true \
+#    ro.qc.sensors.step_detector=true \
+#    ro.qc.sensors.step_counter=true \
+#    ro.qc.sensors.max_geomag_rotvec=true \
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.sensors.qmd=true \
-    ro.qualcomm.sensors.smd=true \
-    ro.qualcomm.sensors.cmc=true \
-    ro.qualcomm.sensors.vmd=true \
-    ro.qualcomm.sensors.gtap=true \
-    ro.qualcomm.sensors.pedometer=true \
-    ro.qualcomm.sensors.pam=true \
-    ro.qualcomm.sensors.scrn_ortn=true \
-    ro.qualcomm.sensors.georv=true \
-    ro.qualcomm.sensors.game_rv=true \
-    ro.qc.sensors.step_detector=true \
-    ro.qc.sensors.step_counter=true \
-    ro.qc.sensors.max_geomag_rotvec=true \
     debug.qualcomm.sns.hal=w \
     debug.qualcomm.sns.daemon=w \
     debug.qualcomm.sns.libsensor1=w
+
+# aDSP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.sensors.gestures=true
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
